ffdf33df9f96ee1e08ed5cef74fa4778a181ee16
Michal Ferber <michal.ferber@ari-integration.com>
2020-11-02 16:53:19 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleEXE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Ouch!", "Clicked Me", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                label1.Text = "Test Success";
+                textBox1.Text = "Ouch!";
+            }
+            else
+            {
+                label1.Text = "Test Canceled";
+                textBox1.Text = "";
+            }
+
+        }
+    }
+}
